c8ff56ec6bb9b78240068c52d237f51088c7f847
Natan <natanjoe@gmail.com>
2025-05-09 15:11:05 -0300

    correçao de cores dos botões para ficar na cor da logo

--- js/products.js
+++ js/products.js
@@ -38,7 +38,7 @@ function renderProducts(products) {
       <img src="${product.imagem}" alt="${product.nome}">
       <h3>${product.nome}</h3>
       <p class="price">R$ ${product.preco.toFixed(2)}</p>
-      <button class="snipcart-add-item my-add-to-cart-btn"
+      <button class="btn snipcart-add-item my-add-to-cart-btn"
         data-item-id="${product.id}"
         data-item-price="${product.preco}"
         data-item-name="${product.nome}"
